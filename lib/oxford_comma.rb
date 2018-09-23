08bf4a79edeec7dc05153b7fbfd99e5c716d7e4e
Grace Choi <oiograceoio@gmail.com>
2018-09-23 03:51:32 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,7 @@ def oxford_comma(array)
     return array.join
   elsif array.size == 2
     array.insert(-1, 'and')
+    return array.join
   elsif array.size > 2
     array.join(" , ")
     array.insert(-1, 'and')
